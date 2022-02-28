d8c5a554b0decb2982bb42974c9830eaed2e580c
Vitaliy <viurum986@mail.ru>
2022-02-28 21:34:31 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿{int a, b;
+Console.WriteLine("Введите два числа : ");
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Минимальное число {0}", a);
+
+}
+else
+{
+    Console.WriteLine("Максимальное число {0}", b);
+}
+Console.ReadLine();}
