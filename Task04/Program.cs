301180cbab2a2fcbb4ff91e1c39610f02b52a146
Vitaliy <viurum986@mail.ru>
2022-02-28 23:23:15 +0300

    TasK04

---
+++ Task04/Program.cs
@@ -0,0 +1,13 @@
+﻿
+    
+    int a = 1;
+    Console.Write("Введите число ");
+    int n = int.Parse(Console.ReadLine());
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write(i + " ");
+        }
+    }
+    Console.ReadLine();
