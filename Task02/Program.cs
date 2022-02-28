1dc0634c816e73412ee68dea5e27f6467685c39f
Vitaliy <viurum986@mail.ru>
2022-02-28 22:15:20 +0300

    TasK02

---
+++ Task02/Program.cs
@@ -0,0 +1,15 @@
+﻿{int a, b, c;
+Console.WriteLine("Введите три числа : ");
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+c = int.Parse(Console.ReadLine());
+
+int max = a;
+if (b > max) max = b;
+
+if (c > max) max = c;
+{
+    Console.WriteLine("Максимальное число {0}", max);
+}
+
+Console.ReadLine();}
